9fe31260b6dc62c14a5de1290d6e9794f9093790
Rob Randall <rob.randall1@gmail.com>
2020-09-20 22:14:15 +0100

    switch to unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Djngo' in browser.title
+class NewViaitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_start_and_retrieve_list_later(self):
+        self.browser.get('http://localhost:8000')
+
+        #header ha To-Do in it
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('finish the test')
+
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
